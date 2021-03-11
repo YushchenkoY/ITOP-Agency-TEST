a0a1cf686b714f35c1bcfd3b0bbba3202976f280
yushchenko yuliia <yushchenko.yu@gmail.com>
2021-03-11 19:27:24 +0200

    Latest version

--- src/App.js
+++ src/App.js
@@ -1,57 +1,84 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { useState } from "react"
-import { interval } from 'rxjs';
-import { startWith, scan} from 'rxjs/operators';
+import { interval, merge,  Subject, NEVER } from 'rxjs';
+import { mapTo, tap, switchMap, startWith, scan, filter, debounceTime, buffer} from 'rxjs/operators';
 
-const createObservable$ = (startValue = 0) => {
-  return interval(1000).pipe(
-    startWith(startValue),
-    scan(t => t + 1)
-  );
-};
+const actions$ = new Subject();
+
+const wait$ = actions$.pipe(
+  filter(action => action === 'wait'),
+  buffer(
+    actions$.pipe(debounceTime(300))
+  ),
+  filter(clickArray => clickArray.length === 2)
+);
+
+const stop$ = actions$.pipe(filter(action => action === 'stop'));
+const start$ = actions$.pipe(filter(action => action === 'start'));
+const reset$ = actions$.pipe(filter(action => action === 'reset'));
+
+
+const events$ = merge(
+  stop$.pipe(mapTo({ count: false, value: 0 })),
+  start$.pipe(mapTo({ count: true})),
+  wait$.pipe(mapTo({ isWaitAction: true })),
+  reset$.pipe(mapTo({ value: 0, count: true })),
+);
 
 function App() {
+  const [isRun, setRun] = useState(false);
+  const [countdownInSeconds, setCountdownValue] = useState(0);
 
-  const [observe, setObserve] = useState(); // состояние подписки
-  const [isRun, setRun] = useState(false); // состояние активности процесса работы
-  const [countdownInSeconds, setCountdownValue] = useState(0); // состояние секундомера 
-  const [runActiv, setRunActiv] = useState(false) // состояние кнопки СТАРТ/СТОП
-
-
-  // Поведение по нажатию кнопок
-  // Старт
-  const startTimer = () => {
-    setRunActiv(true)
-    setRun(true);
-    setObserve(createObservable$()
-      .subscribe((v) => {
-        setCountdownValue(v);
-      }));
+  useEffect(() => {
+    const stopWatch$ = events$.pipe(
+      startWith({
+        count: false,
+        speed: 1000,
+        value: 0,
+        increase: 1
+      }),
+      scan((state, curr) => {
+        if (curr.isWaitAction) {
+          setRun(!state.count);
+          return { ...state, ...curr, count: !state.count };
+        }
+        setRun(curr.count);
+        return { ...state, ...curr };
+      }, {}),
+      tap((state) => setCountdownValue(state.value)),
+      switchMap((state) =>
+        state.count
+          ? interval(state.speed).pipe(
+          tap(
+            _ =>
+              (state.value += state.increase)
+          ),
+          tap(_ => setCountdownValue(state.value))
+          )
+          : NEVER
+      )
+    );
+    const sw = stopWatch$.subscribe();
+    return () => sw.unsubscribe();
+  }, []);
+  
+  const onStart = () => {
+    return actions$.next('start');
   };
-  // Стоп и Сброс
-  const resetTimer = () => {
-    setRunActiv(false)
-    setCountdownValue(0);
-    setRun(false);
-    observe.unsubscribe();
-    setObserve(null);
+  
+  const onStop = () => {
+    return actions$.next('stop');
   };
-  // Ожидание 
-  const waitTimer = () => {
-    if (isRun) {
-      setRun(false);
-      observe.unsubscribe();
-      setObserve(null);
-    } else {
-      setRun(true);
-      setObserve(createObservable$(countdownInSeconds)
-        .subscribe((v) => {
-          setCountdownValue(v);
-        }));
-    }
+  
+  const onWait = () => {
+    return actions$.next('wait');
+  };
+  
+  const onReset = () => {
+    return actions$.next('reset');
   };
 
-  // вывод результата в формате 00:00:00
+
   let hours = Math.floor(countdownInSeconds / 60 / 60);
   let minutes = Math.floor(countdownInSeconds / 60) - (hours * 60);
   let seconds = countdownInSeconds % 60;
@@ -60,19 +87,18 @@ function App() {
     minutes.toString().padStart(2, '0'),
     seconds.toString().padStart(2, '0')
   ].join(':');
-
-
-
+  
+  
   return (
     <div className="App">
       <div className="App-timer"> Time: {clock} </div>
         <div>
-          <button className="App-button" onClick ={isRun ? resetTimer : startTimer}>{runActiv ? 'STOP' : 'START'}</button> 
-          <button className="App-button wait" onClick = {waitTimer}  >WAIT</button>
-          <button className="App-button" onClick={resetTimer}>RESET</button>
+          <button className="App-button" onClick ={isRun ? onStop : onStart}>{isRun ? 'STOP' : 'START'}</button>
+          <button className="App-button" onClick ={isRun ? onWait : () => {}}>WAIT</button>
+          <button className="App-button" onClick={onReset}>RESET</button>
         </div>
     </div>
   );
-};
+}
 
 export default App;
